681fa8e645f7477160a45c58f4e685c2eb4e35ee
Andre Luiz <adventureandre@hotmail.com>
2023-01-10 23:26:07 -0300

    Calcularoda finish

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -31,6 +31,29 @@ export default class Calculator extends Component {
             this.setState({operation, current: 1, clearDisplay: true})
         } else{
             const equals = operation === '='
+            const currentOperation =  this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] = eval(`${values[0]}${currentOperation}${values[1]}`)
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory()
+                    return
+                }
+            }catch (e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: (!equals || values[0] === 0),
+                values
+            })
         }
     }
 
